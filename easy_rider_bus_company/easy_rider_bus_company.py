51c762e226f5ecf1c5eb98826a52fe1c0e0d6046
evylegzhanin <71006851+evylegzhanin@users.noreply.github.com>
2022-05-22 11:21:01 +0300

    Update easy_rider_bus_company.py

--- easy_rider_bus_company/easy_rider_bus_company.py
+++ easy_rider_bus_company/easy_rider_bus_company.py
@@ -1,76 +1,36 @@
-# Write your code here
 import json
-from collections import defaultdict
-import re
+
+data = json.loads(input())
+bus_lines_id = sorted(set([item['bus_id'] for item in data]))
 
 
-def field_val(json_str):
-    dct = defaultdict(int)
-    for pack in json_str:
-        for key, value in pack.items():
-            if key == 'bus_id':
-                if not isinstance(value, int):
-                    dct[key] += 1
-            if key == 'stop_id':
-                if not isinstance(value, int):
-                    dct[key] += 1
-            if key == 'stop_name':
-                if not (isinstance(value, str) and len(value) > 0):
-                    dct[key] += 1
-            if key == 'next_stop':
-                if not isinstance(value, int):
-                    dct[key] += 1
-            if key == 'stop_type':
-                if not isinstance(value, str):
-                    dct[key] += 1
-                else:
-                    if len(value) > 1:
-                        dct[key] += 1
-            if key == 'a_time':
-                if not (isinstance(value, str) and len(value) > 0):
-                    dct[key] += 1
-    return dct, pack
+all_stops = []
+start_stops = []
+finish_stops = []
+
+for bus in bus_lines_id:
+    first_last_stops = []  # temporary list for every bus
+
+    for item in data:
+        if item['bus_id'] == bus:
+            all_stops.append(item['stop_name'])
+            if item['stop_type'] == "S":
+                first_last_stops.append('S')
+                start_stops.append(item['stop_name'])
+            elif item['stop_type'] == "F":
+                first_last_stops.append('F')
+                finish_stops.append(item['stop_name'])
+    if first_last_stops == ['F', 'S'] or first_last_stops == ['S', 'F']:
+        continue
+    else:
+        print(f'There is no start or end stop for the line: {bus}.')
+        exit()
+
+transfer_stops = set([item for item in all_stops if all_stops.count(item) > 1])
 
 
-def format_val(json_str):
-    dct = defaultdict(int)
-    stops_set = '[A-Z].*(Road|Avenue|Boulevard|Street)$'
-    for pack in json_str:
-        for key, value in pack.items():
-            if key == 'stop_name':
-                if not re.match(stops_set, value):
-                    dct[key] += 1
-            if key == 'stop_type':
-                if value not in set(['O', 'S', 'F', '']):
-                    dct[key] += 1
-            if key == 'a_time':
-                template = '([0][0-9]|[1][0-9]|[2][0-3]):[0-5][0-9]$'
-                if not re.match(template, value):
-                    dct[key] += 1
-    keys = ['stop_name', 'stop_type', 'a_time']
-    return dct, keys
-
-
-def stops_number(json_str):
-    dct = defaultdict(int)
-    for pack in json_str:
-        dct[pack['bus_id']] += 1
-    return dct
-
-
-json_inp = json.loads(input())
-dct_fields, keys = field_val(json_inp)
-dct_format, keys_form = format_val(json_inp)
-# print(f'Type and required field validation: {sum(dct_fields.values())} errors')
-# for key in keys.keys():
-#     print(f'{key}: {dct_fields[key]}')
-# print(f'Format validation: {sum(dct_format.values())} errors')
-# for key in keys_form:
-#     print(f'{key}: {dct_format[key]}')
-print(f'Line names and number of stops:')
-dct_stops = stops_number(json_inp)
-print(dct_stops)
-# for key, value in sorted(dct_stops.items()):
-#     print(f'bus_id: {key}, stops: {dct_format[key]}')
-
-
+print(f'''
+Start stops: {len(set(start_stops))} {sorted(set(start_stops))}
+Transfer stops: {len(transfer_stops)} {sorted(transfer_stops)}
+Finish stops: {len(set(finish_stops))} {sorted(set(finish_stops))}
+''')
